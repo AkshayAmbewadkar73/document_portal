a2de0476877eb79b4c4aa926957063a581ea1076
AkshayAmbewadkar73 <akshayambewadkar7350pillu@gmail.com>
2025-10-14 20:48:39 +0530

    code updated for reading pdf

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -4,24 +4,49 @@ import fitz
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
-class DocumentComparator:
-    def  __init__(self):
-        pass
+class DocumentIngestion:
+    
+    def  __init__(self,base_dir):
+        self.log=CustomeLogger().get_logger(__name__)
+        self.base_dir=Path(base_dir)
+        self.base_dir.mkdir(parents=True,exist_ok=True)
 
     def delete_existing_files(self,file_path):
         """
         Delete existing files at specified paths.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing files:{e}")
+            raise DocumentPortalException("An error occured while deleting existing files",sys)
 
     def save_uploadded_file(self):
         """
         Save the uploaded file to the specified directory.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving uploaded file:{e}")
+            raise DocumentPortalException("An error occured while saving the uploaded file",sys)
 
-    def read_pdf(self):
+    def read_pdf(self,pdf_path):
         """
         Read and extract text from a PDF file.
         """
-        pass
+        try:
+            with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError('Pdf is encrypted:{pdf_path.name}')
+                all_text=[]
+                for page_num in range(doc.page_count):
+                    page=doc.load_page(page_num)
+                    text=page.get_text()
+                    if text.strip():
+                        all_text.append(f"\n---page{page_num+1}--\n{text}")
+                self.log.info(f"PDF read successfully",file=str(pdf_path),pages=len(all_text))  
+                return "\n".join(all_text)      
+        except Exception as e:
+            self.log.error(f"Error reading PDF:{e}")
+            raise DocumentPortalException("An error occured while reading the PDF",sys)
--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -11,13 +11,30 @@ from langchain.output_parsers import OutputFixingParser
 
 class DocumentComparatorLLM:
     def __init__(self):
-        pass
+        load_dotenv()
+        self.log=CustomLogger().get_logger(__name__)
+        self.loader=ModelLoader()
+        self.llm=self.loader.load_llm()
+        self.parser=JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser=OutputFixingParser.from_llm(parser=self.parser,llm=self.llm)
+        self.prompt=PROMPT_REGISTRY["document_comparison"]
+        self.chain=self.prompt|self.llm|self.fixing_parser
+        self.log.info("DocumentComparatorLLM initialized successfully")
     def compare_documents(self):
         """
         Compare two documents and returns a structured comparison.
         """
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Error in compare_documents:{e}", error=str(e))
+            raise DocumentPortalException("An error occured while comparing document", sys) from e
     def _format_response(self):
         """
         Format the LLM response into a structured format.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Error formatting response into Dataframe", error=str(e))
+            raise DocumentPortalException("Error formatting response", sys) from e
